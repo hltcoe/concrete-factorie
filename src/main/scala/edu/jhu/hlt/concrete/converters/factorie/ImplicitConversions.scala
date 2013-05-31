cfb522ce92e57c374ef93bedf852c6e8bfb6e1b3
Jack Sullivan <john.t.sullivan@gmail.com>
2013-05-31 13:01:28 -0400

    Switched imports

--- src/main/scala/edu/jhu/hlt/concrete/converters/factorie/ImplicitConversions.scala
+++ src/main/scala/edu/jhu/hlt/concrete/converters/factorie/ImplicitConversions.scala
@@ -1,7 +1,10 @@
 package edu.jhu.hlt.concrete.converters.factorie
 
 import java.util.UUID
-import edu.jhu.hlt.concrete.Concrete
+import edu.jhu.hlt.concrete.Concrete.{Communication, UUID => ConUUID, CommunicationGUID, TextSpan, Token => ConToken,
+                                      TokenTagging, Tokenization, TokenRefSequence, Parse, Sentence => ConSentence,
+                                      SentenceSegmentation, Section, SectionSegmentation}
+import edu.jhu.hlt.concrete.Concrete.Parse.Constituent
 import cc.factorie.app.nlp.{Sentence, Document, Token}
 import scala.collection.JavaConverters._
 import cc.factorie.app.nlp.parse.ParseTree
@@ -19,95 +22,95 @@ object ImplicitConversions {
 
    */
 
-  private def getUUID:Concrete.UUID = {
+  private def getUUID:ConUUID = {
     val uuid = UUID.randomUUID
-    Concrete.UUID.newBuilder.setHigh(uuid.getMostSignificantBits).setLow(uuid.getLeastSignificantBits).build
+    ConUUID.newBuilder.setHigh(uuid.getMostSignificantBits).setLow(uuid.getLeastSignificantBits).build
   }
 
-  implicit def Name2Guid(docName:String):Concrete.CommunicationGUID = Concrete.CommunicationGUID.newBuilder
+  implicit def Name2Guid(docName:String):CommunicationGUID = CommunicationGUID.newBuilder
     .setCorpusName(docName)
     .build
 
-  implicit def Range2TextSpan(rng:Range):Concrete.TextSpan = Concrete.TextSpan.newBuilder
+  implicit def Range2TextSpan(rng:Range):TextSpan = TextSpan.newBuilder
     .setStart(rng.start)
     .setEnd(rng.end)
     .build
 
-  implicit def Fac2ConToken(facTok:Token):Concrete.Token = Concrete.Token.newBuilder
+  implicit def Fac2ConToken(facTok:Token):ConToken = ConToken.newBuilder
     .setTokenId(facTok.sentencePosition)
     .setText(facTok.string)
     .setTextSpan(facTok.stringStart to facTok.stringEnd)
     .build
 
-  private def posTagging(tok:Token):Option[Concrete.TokenTagging.TaggedToken] = Option(tok.posLabel) match {
-    case Some(posLabel) => Some(Concrete.TokenTagging.TaggedToken.newBuilder.setTokenId(tok.sentencePosition).setTag(posLabel.categoryValue).build)
+  private def posTagging(tok:Token):Option[TokenTagging.TaggedToken] = Option(tok.posLabel) match {
+    case Some(posLabel) => Some(TokenTagging.TaggedToken.newBuilder.setTokenId(tok.sentencePosition).setTag(posLabel.categoryValue).build)
     case _ => None
   }
 
-  private def nerTagging(tok:Token):Option[Concrete.TokenTagging.TaggedToken] = Option(tok.nerLabel) match {
-    case Some(nerLabel) => Some(Concrete.TokenTagging.TaggedToken.newBuilder.setTokenId(tok.sentencePosition).setTag(nerLabel.categoryValue).build)
+  private def nerTagging(tok:Token):Option[TokenTagging.TaggedToken] = Option(tok.nerLabel) match {
+    case Some(nerLabel) => Some(TokenTagging.TaggedToken.newBuilder.setTokenId(tok.sentencePosition).setTag(nerLabel.categoryValue).build)
     case _ => None
   }
 
-  private def lemmaTagging(tok:Token):Option[Concrete.TokenTagging.TaggedToken] = Option(tok.lemma) match {
-    case Some(lemma) => Some(Concrete.TokenTagging.TaggedToken.newBuilder.setTokenId(tok.sentencePosition).setTag(lemma.lemma).build)
+  private def lemmaTagging(tok:Token):Option[TokenTagging.TaggedToken] = Option(tok.lemma) match {
+    case Some(lemma) => Some(TokenTagging.TaggedToken.newBuilder.setTokenId(tok.sentencePosition).setTag(lemma.lemma).build)
     case _ => None
   }
 
-  implicit def Tags2Tagging(toks:Iterable[Option[Concrete.TokenTagging.TaggedToken]]):Concrete.TokenTagging = Concrete.TokenTagging.newBuilder
+  implicit def Tags2Tagging(toks:Iterable[Option[TokenTagging.TaggedToken]]):TokenTagging = TokenTagging.newBuilder
     .setUuid(getUUID)
     .addAllTaggedToken(toks.flatten.asJava)
     .build
 
-  implicit def TokenIterable2Tokenization(tokens:Iterable[Token]):Concrete.Tokenization = Concrete.Tokenization.newBuilder
+  implicit def TokenIterable2Tokenization(tokens:Iterable[Token]):Tokenization = Tokenization.newBuilder
     .setUuid(getUUID)
-    .setKind(Concrete.Tokenization.Kind.TOKEN_LIST)
+    .setKind(Tokenization.Kind.TOKEN_LIST)
     .addAllToken((tokens map Fac2ConToken).asJava)
     .addPosTags(tokens map posTagging)
     .addNerTags(tokens map nerTagging)
     .addLemmas(tokens map lemmaTagging)
     .build
 
-  implicit def Index2TokenRefSequence(idex:Int)(implicit uuid:Concrete.UUID):Concrete.TokenRefSequence = Concrete.TokenRefSequence.newBuilder
+  implicit def Index2TokenRefSequence(idex:Int)(implicit uuid:ConUUID):TokenRefSequence = TokenRefSequence.newBuilder
     .addTokenId(idex)
     .setTokenization(uuid)
     .build
 
   //TODO Convert to Dependency Parse
-  private def makeConstituent(id:Int, tree:ParseTree):Concrete.Parse.Constituent = {
-    val con = Concrete.Parse.Constituent.newBuilder
+  private def makeConstituent(id:Int, tree:ParseTree):Constituent = {
+    val con = Constituent.newBuilder
       .setId(id)
       .setTag(tree.label(id).categoryValue)
       .addAllChildren((tree.children(id).map(_.sentencePosition) map {makeConstituent(_,tree)}).asJava)
     if(id == ParseTree.rootIndex) con.build else con.setTokenSequence(id).build
   }
 
-  implicit def Fac2ConParse(facPars:ParseTree):Concrete.Parse = Concrete.Parse.newBuilder
+  implicit def Fac2ConParse(facPars:ParseTree):Parse = Parse.newBuilder
     .setUuid(getUUID)
     .setRoot(makeConstituent(ParseTree.rootIndex, facPars))
     .build
 
-  implicit def Fac2ConSentence(facSent:Sentence):Concrete.Sentence = Concrete.Sentence.newBuilder
+  implicit def Fac2ConSentence(facSent:Sentence):ConSentence = ConSentence.newBuilder
     .setUuid(getUUID)
     .setTextSpan(facSent.start to facSent.end)
     .addTokenization(facSent.toSeq)
     .addParse(facSent.parse)
     .build
 
-  implicit def FacConSentenceSegmentation(sents:Iterable[Sentence]):Concrete.SentenceSegmentation = Concrete.SentenceSegmentation.newBuilder
+  implicit def FacConSentenceSegmentation(sents:Iterable[Sentence]):SentenceSegmentation = SentenceSegmentation.newBuilder
     .setUuid(getUUID)
     .addAllSentence((sents map Fac2ConSentence).asJava)
     .build
 
-  implicit def Document2Communication(doc:Document):Concrete.Communication = Concrete.Communication.newBuilder
+  private def Document2Communication(doc:Document):Communication = Communication.newBuilder
     .setUuid(getUUID)
     .setGuid(doc.name)
     .setText(doc.string)
     .addSectionSegmentation{
-    Concrete.SectionSegmentation.newBuilder // Factorie doesn't split documents into paragraphs
+    SectionSegmentation.newBuilder // Factorie doesn't split documents into paragraphs
       .setUuid(getUUID)
       .addSection{
-      Concrete.Section.newBuilder
+      Section.newBuilder
         .setUuid(getUUID)
         .setTextSpan(0 to doc.stringLength)
         .addSentenceSegmentation(doc.sentences)
@@ -119,7 +122,7 @@ object ImplicitConversions {
   implicit def Document2MyDocument(doc:Document) = new MyDocument(doc)
 
   class MyDocument(doc:Document) {
-    def asCommunication:Concrete.Communication = Document2Communication(doc)
+    def asCommunication:Communication = Document2Communication(doc)
   }
 
   /*
@@ -127,7 +130,7 @@ object ImplicitConversions {
 
    */
 
-  implicit def Communication2Document(comm:Concrete.Communication):Document = {
+  private def Communication2Document(comm:Communication):Document = {
     val id = comm.getGuid
     val text = comm.getText
     val document = new Document(text)
@@ -167,9 +170,9 @@ object ImplicitConversions {
     document
   }
 
-  implicit def Communication2MyCommunication(comm:Concrete.Communication) = new MyCommunication(comm)
+  implicit def Communication2MyCommunication(comm:Communication) = new MyCommunication(comm)
 
-  class MyCommunication(comm:Concrete.Communication) {
+  class MyCommunication(comm:Communication) {
     def asDocument:Document = Communication2Document(comm)
   }
 }
