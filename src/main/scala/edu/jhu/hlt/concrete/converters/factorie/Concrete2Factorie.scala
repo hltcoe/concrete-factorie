0987ab1cff1107827dff8d9b2464337a701630e6
Tan Xu <tanx@umd.edu>
2013-06-03 14:45:16 -0400

    Test read in example concrete data. Change pom to easy calling concrete2factory class Add data/* to gitignore

--- src/main/scala/edu/jhu/hlt/concrete/converters/factorie/Concrete2Factorie.scala
+++ src/main/scala/edu/jhu/hlt/concrete/converters/factorie/Concrete2Factorie.scala
@@ -9,6 +9,8 @@ import cc.factorie._
 import cc.factorie.app.nlp._
 import scala.collection.mutable.ArrayBuffer
 import edu.jhu.hlt.concrete.converters.factorie.ImplicitConversions._
+import scala.util.control.Breaks
+import edu.jhu.hlt.concrete.Concrete.KnowledgeGraph
 
 /**
  * @author tanx
@@ -21,17 +23,34 @@ object Concrete2Factorie {
 	
 	def getFactorieDocs(pathToComm:String): ArrayBuffer[Document]={
 		val commFile = new File(pathToComm)
+		log.info("Reading protobuf file from: "+commFile.getAbsolutePath())
 		val fis = new FileInputStream(commFile)
 		val pbr = new ProtocolBufferReader(fis, classOf[Communication])
+		val knowledge_graph = pbr.next().asInstanceOf[KnowledgeGraph]
 		val docs = new ArrayBuffer[Document]
-		do{ // To-Do:no hasnext() method
-			val curComm = pbr.next().asInstanceOf[Communication]
-			docs.append(curComm.asDocument)
-		}while(true)
-			
+		val loop = new Breaks;
+		loop.breakable{
+			do{ // To-Do:no hasnext() method
+				var nextMessage = pbr.next()
+				log.info(nextMessage.getSerializedSize().toString)
+				if(nextMessage ne null) {
+					val curComm = nextMessage.asInstanceOf[Communication]
+					val document = curComm.asDocument
+					docs.append(document)
+					log.info("Got document: "+document.name)
+				}else loop.break 
+			}while(true)
+		}
+		
 		pbr.close()
         fis.close()
-        
         return docs
+	}
+	
+	def main(args: Array[String]): Unit={
+		var testDat = System.getProperty("testDat")
+		if(testDat eq null) testDat="data/rf-concrete-dump.pb.gz"
+		val documents = getFactorieDocs(testDat)
+		log.info("Got documents: "+documents.size)
 	}
 }
