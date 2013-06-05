0bdb9c01c1b9f55868f64fde5adfdca3e246b862
Jack Sullivan <john.t.sullivan@gmail.com>
2013-06-05 14:08:09 -0400

    Updated to include some postAttrs

--- src/main/scala/edu/jhu/hlt/concrete/converters/factorie/CommunicationAnnotators.scala
+++ src/main/scala/edu/jhu/hlt/concrete/converters/factorie/CommunicationAnnotators.scala
@@ -1,7 +1,9 @@
 package edu.jhu.hlt.concrete.converters.factorie
 
-import cc.factorie.app.nlp.{Document, DocumentAnnotator}
+import cc.factorie.app.nlp.{Document, DocumentAnnotator, Token, Sentence}
 import edu.jhu.hlt.concrete.Concrete.Communication
+import cc.factorie.app.nlp.pos.PTBPosLabel
+import cc.factorie.app.nlp.parse.ParseTree
 
 /**
  * @author John Sullivan
@@ -15,40 +17,33 @@ class CommunicationSegmenter(comm:Communication) extends CommunicationAnnotator(
 
   def prereqAttrs: Iterable[Class[_]] = null
 
-  def postAttrs: Iterable[Class[_]] = null
-}
-class CommunicationTokenizer(comm:Communication) extends CommunicationAnnotator(comm){
-  def process1(document: Document): Document = null
-
-  def prereqAttrs: Iterable[Class[_]] = null
-
-  def postAttrs: Iterable[Class[_]] = null
+  def postAttrs: Iterable[Class[_]] = Vector[Class[_]](classOf[Sentence], classOf[Token])
 }
 class CommunicationPOSTagger(comm:Communication) extends CommunicationAnnotator(comm){
   def process1(document: Document): Document = null
 
   def prereqAttrs: Iterable[Class[_]] = null
 
-  def postAttrs: Iterable[Class[_]] = null
+  def postAttrs: Iterable[Class[_]] = Seq(classOf[PTBPosLabel])
 }
 class CommunicationLemmatizer(comm:Communication) extends CommunicationAnnotator(comm){
   def process1(document: Document): Document = null
 
   def prereqAttrs: Iterable[Class[_]] = null
 
-  def postAttrs: Iterable[Class[_]] = null
+  def postAttrs: Iterable[Class[_]] = null //todo Communication Lemma class?
 }
 class CommunicationNERTagger(comm:Communication) extends CommunicationAnnotator(comm){
   def process1(document: Document): Document = null
 
   def prereqAttrs: Iterable[Class[_]] = null
 
-  def postAttrs: Iterable[Class[_]] = null
+  def postAttrs: Iterable[Class[_]] = //todo Communication NER class?
 }
 class CommunicationDependencyParser(comm:Communication) extends CommunicationAnnotator(comm){
   def process1(document: Document): Document = null
 
   def prereqAttrs: Iterable[Class[_]] = null
 
-  def postAttrs: Iterable[Class[_]] = null
+  def postAttrs: Iterable[Class[_]] = Seq(classOf[ParseTree])
 }
